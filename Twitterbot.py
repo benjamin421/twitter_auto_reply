4e7576fe4eb5148071747f69d220ea6c251c0b93
Joshua Olatunde <josholatunde@gmail.com>
2020-10-15 07:37:43 +0100

    Update Twitterbot.py

--- Twitterbot.py
+++ Twitterbot.py
@@ -16,62 +16,62 @@ cur.execute("SELECT * FROM users")
 data = cur.fetchall()
 
 # while True:
-try:
-  for new_data_me in data:
-        # time.sleep(2)
-        print(new_data_me)
-        token=new_data_me[2]
-        token_secret=new_data_me[3]
-        message=new_data_me[5]
+# try:
+for new_data_me in data:
+      # time.sleep(2)
+      print(new_data_me)
+      token=new_data_me[2]
+      token_secret=new_data_me[3]
+      message=new_data_me[5]
 
 
 
-        API_KEY="VXPUp8XJrpPWthVATRWiqGUvl"
-        API_SECRET="dKDZvazvzsNHDGHipaUbTfOTYDLCNBXUSTm8GjWIO4qJIGAQ8j"
-        ACCESS_TOKEN = token
-        ACCESS_SECRET =token_secret
+      API_KEY="VXPUp8XJrpPWthVATRWiqGUvl"
+      API_SECRET="dKDZvazvzsNHDGHipaUbTfOTYDLCNBXUSTm8GjWIO4qJIGAQ8j"
+      ACCESS_TOKEN = token
+      ACCESS_SECRET =token_secret
 
 
 
-        auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-        auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-        
-        try:
-          api = tweepy.API(auth, wait_on_rate_limit=True,
-              wait_on_rate_limit_notify=True)
+      auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+      auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 
-          time.sleep(2)
-       
-        except:
-          print('couldnotauthenticate')
+      try:
+        api = tweepy.API(auth, wait_on_rate_limit=True,
+            wait_on_rate_limit_notify=True)
 
-        while True:
+        time.sleep(2)
 
-            last_dms = api.list_direct_messages(1)
+      except:
+        print('couldnotauthenticate')
 
-            if last_dms:
-              sender=last_dms[0].message_create['sender_id']
-              print(last_dms)
+      while True:
 
-              try:
-                api.send_direct_message(sender,message)
-                print('sentmessage')
+          last_dms = api.list_direct_messages(1)
 
-              except:
-                print('couldnotsenddm')
+          if last_dms:
+            sender=last_dms[0].message_create['sender_id']
+            print(last_dms)
 
-            else:
-              print('couldnotfetchlastdm')
+            try:
+              api.send_direct_message(sender,message)
+              print('sentmessage')
 
-            break
+            except:
+              print('couldnotsenddm')
+
+          else:
+            print('couldnotfetchlastdm')
+
+          break
 
 
 
-        print('done')
-        time.sleep(10)
+      print('done')
+      time.sleep(10)
 
-except:
-    print('did not work')
+# except:
+    # print('did not work')
 
 
 
